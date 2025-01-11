83a9633d115fea4d161d359930819b3d293fd499
Force-quit <miloulune1@gmail.com>
2025-01-11 13:46:16 -0500

    Updated translations

--- resources/Word-finder_fr.ts
+++ resources/Word-finder_fr.ts
@@ -2,61 +2,66 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR" sourcelanguage="en_US">
 <context>
-    <name>Word-finder</name>
+    <name>QWordFinder</name>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="47"/>
+        <location filename="../src/QWordFinder.cpp" line="49"/>
         <source>Parameters</source>
         <translation>Paramètres</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="51"/>
+        <location filename="../src/QWordFinder.cpp" line="57"/>
         <source>Word list :</source>
         <translation>Liste de mots :</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="53"/>
+        <location filename="../src/QWordFinder.cpp" line="60"/>
         <source>None</source>
         <translation>Aucune</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="55"/>
+        <location filename="../src/QWordFinder.cpp" line="63"/>
         <source>Select file</source>
         <translation>Sélectionner</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="60"/>
+        <location filename="../src/QWordFinder.cpp" line="70"/>
         <source>Max results :</source>
         <translation>Résultats max :</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="83"/>
+        <location filename="../src/QWordFinder.cpp" line="84"/>
         <source>Pattern to find :</source>
         <translation>Motif à trouver :</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="112"/>
+        <location filename="../src/QWordFinder.cpp" line="113"/>
         <source>Copy</source>
         <translation>Copier</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="135"/>
+        <location filename="../src/QWordFinder.cpp" line="139"/>
         <source>File error</source>
         <translation>Erreur de fichier</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="136"/>
+        <location filename="../src/QWordFinder.cpp" line="140"/>
         <source>Error reading file</source>
         <translation>Erreur à la lecture du fichier</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="173"/>
+        <location filename="../src/QWordFinder.cpp" line="191"/>
         <source>Select word list</source>
         <translation>Sélectionnez une liste de mots</translation>
     </message>
     <message>
-        <location filename="../src/QWordFinder.cpp" line="173"/>
-        <source>text files (*.txt)</source>
+        <location filename="../src/QWordFinder.cpp" line="191"/>
+        <source>Text files (*.txt)</source>
         <translation>Fichiers texte (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="../src/QWordFinder.cpp" line="210"/>
+        <source>Please select a word to copy.</source>
+        <translation>Veuillez sélectionner un mot à copier.</translation>
     </message>
 </context>
 </TS>
